a6e1e44ba66113cd58b423bf4ac1158fe40c8a37
Tal Vered <tal.vered@post.runi.ac.il>
2024-11-06 23:29:03 +0200

    question 1, changing the order of the names so it would match the expected output

--- Bill3.java
+++ Bill3.java
@@ -7,7 +7,7 @@ public class Bill3 {
 		String name3 = args[2];
 		double totalCheck = Double.parseDouble(args[3]);
 		double payForEach = Math.ceil(totalCheck / 3); 
-		System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + 
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + 
 							payForEach + " Shekels each.");
 	}
 }
